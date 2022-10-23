05bc9b9c9c878edf815764cab8d999f61b5cbf29
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2022-10-23 23:58:06 +0700

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+      <body>
+
+      <div class="container">
+        <li><a href="#" class="cool-link" id="cool-link">Product</a></li>
+        <li><a href="#" class="cool-link" id="cool-link">Sale</a></li>
+        <li><a href="#" class="cool-link" id="cool-link">About</a></li>
+        <li><a href="#" class="cool-link" id="cool-link">Contact</a></li>
+
+      </div>
+
+      </body>
+    
+    /*{ // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div> }*/
   );
 }
 
